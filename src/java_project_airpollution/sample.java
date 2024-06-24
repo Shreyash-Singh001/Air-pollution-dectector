543135284396efb4de3b72e0a23df301ce66c83e
Shreyash <shreyashsingh3232@gmail.com>
2024-06-24 10:07:37 +0530

    Pushing the file in my repository

---
+++ src/java_project_airpollution/sample.java
@@ -0,0 +1,170 @@
+package java_project_airpollution;
+import java.util.*;
+public class sample {
+    public static void details(int n){
+        if(n==1){
+            System.out.println("The AQI of the city is the:- 104");
+            System.out.println("The PM10 emission is the 22 and the PM 2.5 emission is the:-72");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==2){
+            System.out.println("The AQI of the city is the:- 64");
+            System.out.println("The PM10 emission is the 18 and the PM 2.5 emission is the:-63");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==3){
+            System.out.println("The AQI of the city is the:- 133");
+            System.out.println("The PM10 emission is the 17 and the PM 2.5 emission is the:-61");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==4){
+            System.out.println("The AQI of the city is the:- 97");
+            System.out.println("The PM10 emission is the 28 and the PM 2.5 emission is the:-84");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==5){
+            System.out.println("The AQI of the city is the:- 54");
+            System.out.println("The PM10 emission is the 13 and the PM 2.5 emission is the:-50");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==6){
+            System.out.println("The AQI of the city is the:- 54");
+            System.out.println("The PM10 emission is the 12 and the PM 2.5 emission is the:-46");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==7){
+            System.out.println("The AQI of the city is the:- 53");
+            System.out.println("The PM10 emission is the 13 and the PM 2.5 emission is the:-53");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==8){
+            System.out.println("The AQI of the city is the:- 33");
+            System.out.println("The PM10 emission is the 8 and the PM 2.5 emission is the:-33");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==9){
+            System.out.println("The AQI of the city is the:- 38");
+            System.out.println("The PM10 emission is the 9 and the PM 2.5 emission is the:-38");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==10){
+            System.out.println("The AQI of the city is the:- 50");
+            System.out.println("The PM10 emission is the 12 and the PM 2.5 emission is the:-50");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==11){
+            System.out.println("The AQI of the city is the:- 44");
+            System.out.println("The PM10 emission is the 12 and the PM 2.5 emission is the:-50");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==12){
+            System.out.println("The AQI of the city is the:- 97");
+            System.out.println("The PM10 emission is the 29 and the PM 2.5 emission is the:-82");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==13){
+            System.out.println("The AQI of the city is the:- 50");
+            System.out.println("The PM10 emission is the 12 and the PM 2.5 emission is the:-46");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==14){
+            System.out.println("The AQI of the city is the:- 55");
+            System.out.println("The PM10 emission is the 11 and the PM 2.5 emission is the:-46");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==15){
+            System.out.println("The AQI of the city is the:- 38");
+            System.out.println("The PM10 emission is the 13 and the PM 2.5 emission is the:-52");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==16){
+            System.out.println("The AQI of the city is the:- 46");
+            System.out.println("The PM10 emission is the 11 and the PM 2.5 emission is the:-46");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==17){
+            System.out.println("The AQI of the city is the:- 127");
+            System.out.println("The PM10 emission is the 20 and the PM 2.5 emission is the:-68");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==18){
+            System.out.println("The AQI of the city is the:- 82");
+            System.out.println("The PM10 emission is the 23 and the PM 2.5 emission is the:-78");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==19){
+            System.out.println("The AQI of the city is the:- 57");
+            System.out.println("The PM10 emission is the 8 and the PM 2.5 emission is the:-40");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==20){
+            System.out.println("The AQI of the city is the:- 60");
+            System.out.println("The PM10 emission is the 4 and the PM 2.5 emission is the:-36");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==21){
+            System.out.println("The AQI of the city is the:- 97");
+            System.out.println("The PM10 emission is the 29 and the PM 2.5 emission is the:-87");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==22){
+            System.out.println("The AQI of the city is the:- 66");
+            System.out.println("The PM10 emission is the 18 and the PM 2.5 emission is the:-63");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else if(n==23){
+            System.out.println("The AQI of the city is the:- 64");
+            System.out.println("The PM10 emission is the 3 and the PM 2.5 emission is the:-42");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==24){
+            System.out.println("The AQI of the city is the:- 46");
+            System.out.println("The PM10 emission is the 11 and the PM 2.5 emission is the:-46");
+            System.out.println("The quality of the air is:- GOOD");
+        }
+        else if(n==25){
+            System.out.println("The AQI of the city is the:- 84");
+            System.out.println("The PM10 emission is the 38 and the PM 2.5 emission is the:-15");
+            System.out.println("The quality of the air is:- MODERATE");
+        }
+        else{
+            System.out.println("PLease enter valid input");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("WELCOME TO THE AIR POLLUTION MONITERING SYSTEM");
+        System.out.println("Here we provide all the details anout the AQI and PPM of the all the districts present in the Chennai");
+        System.out.println(
+                "\t1).Chennai\n\t"+
+                "2).Coimbatore\n\t"+
+                "3).Conjeeveram\n\t"+
+                "4).Cuddalore\n\t"+
+                "5).indigul\n\t"+
+                "6).rode\n\t"+
+                "7).Karur\n\t"+
+                "8).Kilkunda\n\t"+
+                "9).Kodaikanal\n\t"+
+                "10).Madurai\n\t"+
+                "11).Nagercoil\n\t"+
+                "12).Negapatam\n\t"+
+                "13).Ootacamund\n\t"+
+                "14).Rajapalaiyam\n\t"+
+                "15).Rameswaram\n\t"+
+                "16).Salem\n\t"+
+                "17).Seven Pagodas\n\t"+
+                "18).Tanjore\n\t"+
+                "19).Tinnevelly\n\t"+
+                "20).Tiruppur\n\t"+
+                "21).Tiruvannamalai\n\t"+
+                "22).Trichinopoly\n\t"+
+                "23).Tuticorin\n\t"+
+                "24).Valparai\n\t"+
+                "25).Vellore\n\t");
+        System.out.println("Enter your input in the form of the index number");
+        int n=sc.nextInt();
+        details(n);
+
+    }
+}
